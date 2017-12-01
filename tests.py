919ef5b0017bdbabf112bc2427c0c5848db09045
chike <chike@sfc.wide.ad.jp>
2017-12-01 15:50:13 +0900

    fix test

--- tests.py
+++ tests.py
@@ -11,8 +11,6 @@ def test_makeblock():
         bcs.make_tx()
         bcs.make_block(100)
 
-    bcs.bc.chain[2]["previous_hash"] = hashlib.sha256("hoge".encode("utf-8")).hexdigest()
-
     for a in range(1,len(bcs.bc.chain)):
         previous = json.dumps(bcs.bc.chain[a-1])
         assert bcs.bc.chain[a]["previous_hash"] == hashlib.sha256(previous.encode('utf-8')).hexdigest()
