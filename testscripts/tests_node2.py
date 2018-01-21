f9899aa3a9dd33c483aa2eed20e8e739a2df2368
chike <chike@sfc.wide.ad.jp>
2018-01-21 19:50:21 +0900

    simple orphan test

--- testscripts/tests_node2.py
+++ testscripts/tests_node2.py
@@ -2,7 +2,14 @@
 import sys
 from nodemanager import *
 
+def test_send_orphan(node):
+    node.make_block(100)
+    node.make_block(100)
+    node.make_block(100)
+    node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
+    node.make_block(100)
+
 args = sys.argv
 node = NodeManager(args[1])
-node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
-node.make_block(100)
+
+test_send_orphan(node)
