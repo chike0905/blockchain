6ae8abbfd0bad3a05ca9bf25183968cd820c5d8e
chike <chike@sfc.wide.ad.jp>
2018-01-22 12:05:02 +0900

    make test case

--- testscripts/tests_node1.py
+++ testscripts/tests_node1.py
@@ -2,10 +2,14 @@
 import sys
 from nodemanager import *
 
+def test_orphan(node):
+    node.make_block(100)
+
 def test_orphan_and_conflict(node):
     node.make_tx()
     node.make_block(100)
 
 args = sys.argv
 node = NodeManager(args[1])
-
+test_orphan(node)
+#test_orphan_and_conflict(node)
--- testscripts/tests_node2.py
+++ testscripts/tests_node2.py
@@ -2,38 +2,46 @@
 import sys
 from nodemanager import *
 
+def test_make_block(node, score, times):
+    for a in range(0,times):
+        node.make_block(score)
+
+def test_make_tx(node, times):
+    for a in range(0,times):
+        node.make_tx()
 
 def test_make_tx_and_send(node):
     node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
-    node.make_tx()
-    node.make_tx()
-    node.make_tx()
+    test_make_tx(node, 3)
 
-def test_make_block_and_tx(node):
-    node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
-    node.make_tx()
-    node.make_block(100)
-    node.make_tx()
-    node.make_block(100)
-    node.make_tx()
-    node.make_block(100)
+def test_make_block_and_tx(node, score, times):
+    for a in range(0,times):
+        node.make_tx()
+        node.make_block(score)
 
 def test_send_orphan(node):
-    node.make_block(100)
-    node.make_block(100)
-    node.make_block(100)
+    test_make_block(node, 100, 3)
     node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
     node.make_block(100)
 
-def test_send_orphan_and_conflicts(node):
-    node.make_block(100)
-    node.make_block(100)
-    node.make_block(100)
+def test_conflict_correct(node):
     node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
-    node.make_block(100)
+    test_make_block_and_tx(node, 1000, 1)
+
+
+def test_conflict_not_correct(node):
+    node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
+    test_make_block_and_tx(node, 10, 1)
+
+def test_send_orphan_and_conflicts(node):
+    test_make_block_and_tx(node, 1000, 3)
+    node.msgmng.add_peer({"addr":"10.2.0.2", "port":5555})
+    node.make_block(1000)
 
 args = sys.argv
 node = NodeManager(args[1])
 
-test_make_block_and_tx(node)
-print(node.chainmng.txmng.txpool)
+#test_send_orphan(node)
+#test_conflict_correct(node)
+#test_conflict_not_correct(node)
+test_send_orphan_and_conflicts(node)
